0986b017198816511df8c305863a2105f89c319c
kis6905 <kis6905@gmail.com>
2022-11-21 22:44:42 +0900

    modify the public path

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/front-test/sw.js', { scope: '/front-test/' })})}
